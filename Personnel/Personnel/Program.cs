ea94a4e0fcee2f7dee721cfb1d60799781d6b9e3
ThaliaDeShalit <thaliapenguin@gmail.com>
2016-07-02 20:20:29 +0300

    created lab 9 answer file, as well as finished personnel app

---
+++ Personnel/Personnel/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Personnel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/Users/Thalia/Desktop/CodeValue/HW/Personnel/TestFile.txt";
+
+            StreamReader sr = File.OpenText(path);
+
+            List<string> dataFromFile = new List<string>();
+
+            string s;
+            while ((s = sr.ReadLine()) != null)
+            {
+                dataFromFile.Add(s);
+            }
+
+            foreach (string str in dataFromFile)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
